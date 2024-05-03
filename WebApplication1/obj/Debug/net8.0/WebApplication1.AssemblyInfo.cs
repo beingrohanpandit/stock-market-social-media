139937bb9dffb6a2bf32ff89bf3941164235d430
beingrohanpandit <rohansharma12434@gmail.com>
2024-05-03 15:32:32 +0530

    add dependency

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861b2d29beead406bbd14f5fd54844a919475812")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43d80e3bc549fc1783d7a6a241ba8510c2cc387")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
