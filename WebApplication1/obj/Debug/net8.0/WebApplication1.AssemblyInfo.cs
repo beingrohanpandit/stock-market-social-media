8cee58b769d16c537eb0ea84afe5580f6100779a
beingrohanpandit <rohansharma12434@gmail.com>
2024-05-03 17:04:32 +0530

    feat(comment): Adding validation in update and create api endpoints

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4801d0ab908d8fbc73d2db011e282ae5eba517cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0dad8a1d1705a833792a596e20decca46fdeb7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
